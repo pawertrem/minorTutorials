12213f8eb0eced3ef3f3918a7a64a146f91f0a51
Павел Иванов <pavelivanov@Noutbuk-Pavel.local>
2025-04-06 23:14:02 +0300

    Initial commit of minorTutorials package

---
+++ R/data-documentation.R
@@ -0,0 +1,149 @@
+#' @docType data
+#' @title Migration Network Data
+#' @description Directed and weighted adjacency matrix of international migration flows.
+#' @format A data frame with 10 rows and 10 variables.
+#' @source AdjacencyDirectedWeighted_14.csv
+#' @examples
+#' data(migration_data)
+#' head(migration_data)
+'migration_data'
+
+#' @docType data
+#' @title Netflix Ratings Data
+#' @description Netflix user rating dataset.
+#' @format A data frame with 1000 rows and 7 variables.
+#' @source netflix.xlsx
+#' @examples
+#' data(netflix_data)
+#' str(netflix_data)
+'netflix_data'
+
+#' @docType data
+#' @title Netflix Titles Metadata
+#' @description Information about Netflix titles (movies and shows).
+#' @format A data frame with 6234 rows and 12 variables.
+#' @source netflix_titles.csv
+#' @examples
+#' data(netflix_titles)
+#' head(netflix_titles)
+'netflix_titles'
+
+#' @docType data
+#' @title Jane Eyre Text
+#' @description Textual dataset from the novel "Jane Eyre".
+#' @format A data frame with 21005 rows and 2 variables.
+#' @source Jane_Eyre_13.csv
+#' @examples
+#' data(Jane_Eyre)
+#' head(Jane_Eyre)
+'Jane_Eyre'
+
+#' @docType data
+#' @title Terrorism Dataset
+#' @description Global terrorism incidents data.
+#' @format A data frame with 181691 rows and 12 variables.
+#' @source data_7.csv
+#' @examples
+#' data(terrorism_data)
+#' summary(terrorism_data)
+'terrorism_data'
+
+#' @docType data
+#' @title Davis Friendship Network
+#' @description Friendship relations in a small group.
+#' @format A data frame with 89 rows and 2 variables.
+#' @source davis_25.csv
+#' @examples
+#' data(davis_data)
+#' head(davis_data)
+'davis_data'
+
+#' @docType data
+#' @title Drug Deaths Data
+#' @description Statistics on drug-related deaths across regions.
+#' @format A data frame with 11981 rows and 48 variables.
+#' @source DrugsDeaths_5.csv
+#' @examples
+#' data(drugs_data)
+#' summary(drugs_data)
+'drugs_data'
+
+#' @docType data
+#' @title Florentine Families Network
+#' @description Relationships among Renaissance Florentine families.
+#' @format A data frame with 16 rows and 16 variables.
+#' @source florentineFamily_2.csv
+#' @examples
+#' data(florentine_data)
+#' florentine_data
+'florentine_data'
+
+#' @docType data
+#' @title HSE One Love Survey
+#' @description Survey of students on topics of love and relationships.
+#' @format A data frame with 450 rows and 3 variables.
+#' @source HSE_one_love_23.csv
+#' @examples
+#' data(HSE_one_love)
+#' head(HSE_one_love)
+'HSE_one_love'
+
+#' @docType data
+#' @title IMDb Top 1000 Movies
+#' @description Metadata on top 1000 movies from IMDb.
+#' @format A data frame with 979 rows and 6 variables.
+#' @source imdb_1000_23.csv
+#' @examples
+#' data(imdb_1000)
+#' head(imdb_1000)
+'imdb_1000'
+
+#' @docType data
+#' @title Newman Network 1
+#' @description Network data used in community detection.
+#' @format A data frame with 58594 rows and 2 variables.
+#' @source newman1_25.txt
+#' @examples
+#' data(newman1)
+#' head(newman1)
+'newman1'
+
+#' @docType data
+#' @title Newman Network 2
+#' @description Second network for testing graph algorithms.
+#' @format A data frame with 16725 rows and 3 variables.
+#' @source newman2_25.txt
+#' @examples
+#' data(newman2)
+#' head(newman2)
+'newman2'
+
+#' @docType data
+#' @title Florentine Party Invitations
+#' @description Party invitation network among Florentine elite.
+#' @format A data frame with 16 rows and 3 variables.
+#' @source party_9.csv
+#' @examples
+#' data(florentine_party)
+#' florentine_party
+'florentine_party'
+
+#' @docType data
+#' @title Predimed Nutrition Study
+#' @description Data from a nutritional intervention trial (PREDIMED).
+#' @format A data frame with 6324 rows and 15 variables.
+#' @source predimed_18.csv
+#' @examples
+#' data(predimed_data)
+#' summary(predimed_data)
+'predimed_data'
+
+#' @docType data
+#' @title Tales Tutorial Data
+#' @description Simple dataset used for demonstration purposes.
+#' @format A data frame with 10 rows and 1 variable.
+#' @source tales_tutorial_3.csv
+#' @examples
+#' data(tales_data)
+#' tales_data
+'tales_data'
